634c8b70c7a030ac5703828402a00994eb0fb42c
Reynaldo Gil <gilcu2@gmail.com>
2016-04-30 22:58:25 +0200

    counter ok

--- app/src/main/scala/com/gilcu2/wearblackscala/MainActivityScala.scala
+++ app/src/main/scala/com/gilcu2/wearblackscala/MainActivityScala.scala
@@ -3,20 +3,30 @@ package com.gilcu2.wearblackscala
 import android.app.Activity
 import android.os.Bundle
 import android.support.wearable.view.WatchViewStub
-import android.widget.TextView
+import android.view.View
+import android.widget.{Button, TextView}
 
 /**
  * Created by gilcu2 on 4/30/16.
  */
 class MainActivityScala extends Activity {
+
+  var count = 0
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     val stub: WatchViewStub = findViewById(R.id.watch_view_stub).asInstanceOf[WatchViewStub]
     stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
       def onLayoutInflated(stub: WatchViewStub) {
-        val mTextView = stub.findViewById(R.id.text).asInstanceOf[TextView]
-        mTextView.append(" from Scala")
+        val text_counter = stub.findViewById(R.id.count_text).asInstanceOf[TextView]
+        val hellow_button = stub.findViewById(R.id.hellow_button).asInstanceOf[Button]
+        text_counter.append(" from Scala")
+        hellow_button.setOnClickListener(new View.OnClickListener() {
+          def onClick(v: View) = {
+            count += 1
+            text_counter.setText(count.toString)
+          }
+        })
       }
     })
   }
